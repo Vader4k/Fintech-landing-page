2fb0412a367e8cb23ddea2cd2b4946630e77b634
danie odukwe <kingdanny295@gmail.com>
2024-08-17 22:25:49 +0100

    done animation for testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,11 @@
+"use client"
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 
 const testimonials = [
   {
@@ -37,9 +40,22 @@ export const Testimonials = () => {
       <div className="container">
         <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations</h2>
         <p className="text-white/70 text-lg md:text-xl text-center max-w-sm mt-5 tracking-tight mx-auto">Our revolutionary AI SEO tools have transformed our client's strategies.</p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex gap-5">
-            {testimonials.map((testimonial) => (
+        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)] flex">
+          <motion.div 
+            className="flex gap-5 pr-5 flex-none -translate-x-1/2"
+            initial={{
+              translateX: '-50%',
+            }}
+            animate={{
+              translateX: '0',
+            }}
+            transition={{
+              duration: 30,
+              ease: 'linear',
+              repeat: Infinity,
+            }}
+          >
+            {[...testimonials, ...testimonials].map((testimonial) => (
               <div 
                 key={testimonial.name}
                 className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
@@ -62,7 +78,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
