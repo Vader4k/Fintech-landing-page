98357cc558f571df56dc11ebf9e5e427718655e1
danie odukwe <kingdanny295@gmail.com>
2024-08-18 20:12:04 +0100

    done for now

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -39,7 +39,9 @@ export const Testimonials = () => {
     <section className="py-20 md:py-24">
       <div className="container">
         <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations</h2>
-        <p className="text-white/70 text-lg md:text-xl text-center max-w-sm mt-5 tracking-tight mx-auto">Our revolutionary AI SEO tools have transformed our client's strategies.</p>
+        <p className="text-white/70 text-lg md:text-xl text-center max-w-sm mt-5 tracking-tight mx-auto">
+          Our revolutionary AI SEO tools have transformed our client's strategies.
+        </p>
         <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)] flex">
           <motion.div 
             className="flex gap-5 pr-5 flex-none -translate-x-1/2"
@@ -55,7 +57,7 @@ export const Testimonials = () => {
               repeat: Infinity,
             }}
           >
-            {[...testimonials, ...testimonials].map((testimonial) => (
+            {[...testimonials,...testimonials].map((testimonial) => (
               <div 
                 key={testimonial.name}
                 className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
