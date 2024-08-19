69254014e78707512d6ea9f274cc7e1554e6b167
danie odukwe <kingdanny295@gmail.com>
2024-08-19 15:09:48 +0100

    added mouse position animation to CTA section using refs and custom hook to set and get mouse position of the required div and useEffect to handle the state change of the x and y property of the div

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,22 +1,83 @@
+"use client"
 import Button from "@/components/Button";
 import starsBg from '@/assets/stars.png'
 import gridLines from '@/assets/grid-lines.png'
+import { motion, useMotionTemplate, useMotionValue, useScroll, useTransform } from 'framer-motion';
+import { RefObject, useEffect, useRef } from "react";
+
+const useRelativeMouseValue = (to: RefObject<HTMLElement>) => {
+  const mouseX = useMotionValue(0)
+  const mouseY = useMotionValue(0)
+
+  const updateMousePosition = (event: MouseEvent) => {
+    if (!to.current) return;
+    const { top, left } = to.current.getBoundingClientRect();
+    mouseX.set(event.clientX - left)
+    mouseY.set(event.clientY - top)
+  }
+
+  useEffect(() => {
+    const element = to.current;
+    if (!element) return;
+
+    element.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      element.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, [to]);
+
+
+  return [mouseX, mouseY]
+}
+
 
 export const CallToAction = () => {
+
+  const scrollRef = useRef<HTMLElement>(null)
+  const divRef = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: scrollRef,
+    offset: ['start end', 'end start']
+  })
+
+  const translateY = useTransform(scrollYProgress, [0,1], [-300, 300])
+
+  const [mouseX, mouseY] = useRelativeMouseValue(divRef)
+
+  const maskImage = useMotionTemplate `radial-gradient(ellipse 50% 50% at ${mouseX}px ${mouseY}px,black,transparent)`
+
+
   return (
-    <section className="py-20 md:py-24">
+    <motion.section className="py-20 md:py-24" ref={scrollRef}>
       <div className="container">
-        <div 
-          className="border border-white/5 py-24 rounded-xl overflow-hidden relative" 
+        <motion.div
+          ref={divRef} 
+          className="border border-white/15 py-24 rounded-xl overflow-hidden relative group" 
           style={{
             backgroundImage: `url(${starsBg.src})`,
-            backgroundSize: 'cover',
+            backgroundPositionY: translateY
+          }}
+          animate={{
+            backgroundPositionX: starsBg.width
+          }}
+          transition={{
+            duration: 60,
+            repeat: Infinity,
+            ease: 'linear',
           }}
         >
           <div 
-            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]"
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay [mask-image:radial-gradient(ellipse_50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700"
             style={{
-              backgroundImage: `url(${gridLines.src})`
+              backgroundImage: `url(${gridLines.src})`,
+            }}
+          />
+          <motion.div 
+            className="absolute inset-0 bg-[rgb(74,32,138)] bg-blend-overlay opacity-0 group-hover:opacity-100 transition duration-700"
+            style={{
+              backgroundImage: `url(${gridLines.src})`,
+              maskImage
             }}
           />
           <div className="relative">
@@ -28,8 +89,8 @@ export const CallToAction = () => {
               </Button>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 };
